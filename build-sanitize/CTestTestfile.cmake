681f2d0b299f11ddc7b4b5114adcef689bd38d4b
Yong Wang <y.wang6@student.vu.nl>
2026-09-24 19:26:15 +0200

    Remove sanitizer build artifacts from repository

--- build-sanitize/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user1/low-latency-trading-system
-# Build directory: /home/user1/low-latency-trading-system/build-sanitize
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/user1/low-latency-trading-system/build-sanitize/matching_tests_e3b0c442_include.cmake")
-subdirs("_deps/googletest-build")
